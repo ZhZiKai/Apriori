93f2ba9cc898b0fe2764bcc3b200471f713a40aa
ZhZiKai <zzkde@qq.com>
2017-03-19 21:32:49 +0800

    Apriori算法,控制台输出频繁集.

---
+++ src/Client.java
@@ -0,0 +1,14 @@
+/**
+ * apriori关联规则挖掘算法调用类
+ * @author lyq
+ *
+ */
+public class Client {
+	public static void main(String[] args){
+		//String filePath = "C:\\Users\\lyq\\Desktop\\icon\\testInput.txt";
+		String filePath = "C:\\Users\\zzk\\Desktop\\testInput.txt";
+		
+		AprioriTool tool = new AprioriTool(filePath, 2);
+		tool.printAttachRule(0.7);
+	}
+}
